d8705942bda84ff49ff8424da33124f30fb0764b
Makrela <89453147+MysterRogby@users.noreply.github.com>
2023-01-14 16:28:49 +0100

    Delete line_reverser directory

--- line_reverser/line_reverser.py
+++
@@ -1,9 +0,0 @@
-file = open("input.txt")
-lines = file.readlines()
-lines[-1] = lines[-1] + "\n"
-lines.reverse()
-newfile = open("result.txt", "w")
-newfile.writelines(lines)
-
-file.close()
-newfile.close()
